8a5fbd5dfba9c170693ab2bd84d9cbc0180306d7
manuelsernaeugenio <mansereug@alu.edu.gva.es>
2022-09-15 19:34:00 +0200

    Segundo commit

---
+++ src/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-i">
+    <title>Hola Mundo</title>
+</head>
+<body>
+    <?php
+        echo "¡Hola Mundo!";
+    ?>
+</body>
+</html>
